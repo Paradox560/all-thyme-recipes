0782c93cf250009e8de671b3abc3c16a656ec633
Paradox560 <65742681+Paradox560@users.noreply.github.com>
2024-09-29 01:43:11 -0400

    Disable eslint unused vars

--- src/app/generate-recipes/page.tsx
+++ src/app/generate-recipes/page.tsx
@@ -1,4 +1,5 @@
 export default function Page() {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const expirationPrompt = `You are a food specialist, you take in the purchase date of each item and return the expiration date of each item from it. Make sure to not skip any items.
         The "purchaseDate" should be the date that is passed in, and the "expirationDate" should be the estimated expiration date for each product.
         Make sure to only return one date in the format of mm/dd. If you are unable to find a singular date then return "NaN".
@@ -12,7 +13,7 @@ export default function Page() {
             ]
         }
     `
-    
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const recipePrompt = `
         You are a food specialist, you take in all of the ingredients and their respective expiration dates and return a list of recipes that can be made with these ingredients.
         Try to prioritize including the items with the earliest expiration dates but this is not a hard requirement, you can choose from any of the ingredients provided but only these.
