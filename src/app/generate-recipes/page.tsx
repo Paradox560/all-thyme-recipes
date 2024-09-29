b74e06a277dba5fe4b9aa9258a965684318aa487
Paradox560 <65742681+Paradox560@users.noreply.github.com>
2024-09-29 09:18:58 -0400

    Fixed Gemini

--- src/app/generate-recipes/page.tsx
+++ src/app/generate-recipes/page.tsx
@@ -1,7 +1,10 @@
 'use client'
 
+import { useUser } from "@clerk/nextjs"
+import { Button } from "@mui/material";
+
 export default function Page() {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { isLoaded, isSignedIn, user } = useUser();
     const expirationPrompt = `You are a food specialist, you take in the purchase date of each item and return the expiration date of each item from it. Make sure to not skip any items.
         The "purchaseDate" should be the date that is passed in, and the "expirationDate" should be the estimated expiration date for each product.
         Make sure to only return one date in the format of mm/dd. If you are unable to find a singular date then return "NaN".
@@ -9,13 +12,14 @@ export default function Page() {
         {
             info:[
                 {
-                "purchaseDate": str,
-                "expirationDate": str
+                    "name": str,
+                    "purchaseDate": str,
+                    "expirationDate": str
                 }
             ]
         }
+        Where name is the name of the product passed in, purchaseDate is the purchaseDate passed in for each product, and the generated expirationDate
     `
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const recipePrompt = `
         You are a food specialist, you take in all of the ingredients and their respective expiration dates and return a list of recipes that can be made with these ingredients.
         Try to prioritize including the items with the earliest expiration dates but this is not a hard requirement, you can choose from any of the ingredients provided but only these.
@@ -33,5 +37,31 @@ export default function Page() {
             ]
         }
     `
-    return <h1>Hello, Generate Recipes Page!</h1>
+
+    const handleSubmit = async (systemPrompt: string, userPrompt: string) => {
+        const requestBody = {
+            systemPrompt: systemPrompt,
+            userPrompt: userPrompt
+        };
+
+        fetch('api/generate', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(requestBody),
+        })
+        .then((res) => res.json())
+        .then((data) => {
+            console.log(data);
+        })
+    };
+    
+   
+    return (
+        <Button sx={{ flex: 1 }} variant='contained' color='primary' onClick={() => handleSubmit(expirationPrompt, 'Eggs - 09/29, Bread - 09/28')} >
+                {' '}
+                Submit
+        </Button>
+    )
 }
