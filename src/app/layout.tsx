21b49c452ba17febcc1c5bfaf1b32e44479b1527
Paradox560 <65742681+Paradox560@users.noreply.github.com>
2024-09-29 01:58:21 -0400

    Added Clerk publishable key field

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
+    <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
       <html lang="en">
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
