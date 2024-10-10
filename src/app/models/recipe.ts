c4812ef96118fb46898187a48652a18e6b8265c8
Paradox560 <65742681+Paradox560@users.noreply.github.com>
2024-10-10 16:12:28 -0400

    Remove mongodb files since we switched to firebase

--- src/app/models/recipe.ts
+++
@@ -1,6 +0,0 @@
-import { ObjectId } from "mongodb";
-
-
-export default class User {
-    constructor(public user_first_name: string, public user_last_name: string, public user_email: string, public ingredients: object[],  public id?: ObjectId) {}
-}
