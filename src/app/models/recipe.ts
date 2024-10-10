18a0c9a0f3e3fb380d95f806bb883775906122e6
Paradox560 <65742681+Paradox560@users.noreply.github.com>
2024-10-10 16:08:57 -0400

    change typing

--- src/app/models/recipe.ts
+++ src/app/models/recipe.ts
@@ -2,5 +2,5 @@ import { ObjectId } from "mongodb";
 
 
 export default class User {
-    constructor(public user_first_name: string, public user_last_name: string, public user_email: string, public ingredients: Object[],  public id?: ObjectId) {}
+    constructor(public user_first_name: string, public user_last_name: string, public user_email: string, public ingredients: object[],  public id?: ObjectId) {}
 }
