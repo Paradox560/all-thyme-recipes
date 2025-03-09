3e5e00c1b0c3e3d3b9da2409598e3ff4986e0a5f
Paradox560 <65742681+Paradox560@users.noreply.github.com>
2025-03-09 18:31:49 -0400

    Added recipe instructions modal to recipe generation

--- src/app/generate-recipes/page.tsx
+++ src/app/generate-recipes/page.tsx
@@ -1,53 +1,57 @@
-'use client'
-import { useEffect, useState } from 'react';
+"use client";
+import { useEffect, useState } from "react";
 import { Oooh_Baby } from "next/font/google";
 import { Typography, Box, Stack, Button, Alert } from "@mui/material";
 import Navbar from "../components/Navbar";
 import { Jost } from "next/font/google";
-import { useUser } from '@clerk/nextjs';
-import { db } from '@/firebase';
-import { doc, collection, getDoc } from 'firebase/firestore';
-import RecipeCard from './recipeCard';
+import { useUser } from "@clerk/nextjs";
+import { db } from "@/firebase";
+import { doc, collection, getDoc } from "firebase/firestore";
+import RecipeCard from "./recipeCard";
 
 const oooh_baby = Oooh_Baby({
-    weight: '400',
-    style: 'normal',
-    subsets: ['latin'],
+  weight: "400",
+  style: "normal",
+  subsets: ["latin"],
 });
 
 const jost = Jost({
-    weight: ['300', '400', '500'],
-    subsets: ['latin'],
+  weight: ["300", "400", "500"],
+  subsets: ["latin"],
 });
 
 interface IngredientData {
-    amount: string;
-    purchaseDate: string;
-    expirationDate: string;
+  amount: string;
+  purchaseDate: string;
+  expirationDate: string;
 }
 
 interface RecipeData {
-    name: string;
-    image: string;
-    description: string;
-    additionalIngredients?: string;
-    link: string;
+  name: string;
+  image: string;
+  description: string;
+  additionalIngredients?: string;
+  link: string;
+  recipesInstructions?: string[];
 }
 
 export default function Page() {
-    const [showRecipes, setShowRecipes] = useState(false);
-    const [ingredients, setIngredients] = useState<Record<string, IngredientData>>({});
-    const [data, setData] = useState<RecipeData[]>([]);
-    const [spiceStates, setSpiceStates] = useState<string[]>([]);
-    const [error, setError] = useState<string>('');
+  const [showRecipes, setShowRecipes] = useState(false);
+  const [ingredients, setIngredients] = useState<
+    Record<string, IngredientData>
+  >({});
+  const [data, setData] = useState<RecipeData[]>([]);
+  const [spiceStates, setSpiceStates] = useState<string[]>([]);
+  const [error, setError] = useState<string>("");
 
-    const { isLoaded, isSignedIn, user } = useUser();
+  const { isLoaded, isSignedIn, user } = useUser();
 
-    const currentIngredientRecipePrompt = `
+  const currentIngredientRecipePrompt = `
         You are a food specialist, you take in all of the ingredients and their respective expiration dates and return a list of recipes that can be made with these ingredients.
         Try to prioritize including the items with the earliest expiration dates but this is not a hard requirement, you can choose from any of the ingredients provided but only these.
         The "name" should be the name of the meal, the "image" should be the image of the meal in the form of the image address that can be used in a Image tag,
-        the "description" should be the description of the meal, and finally the "link" is the link to the recipe website with steps to make the meal.
+        the "description" should be the description of the meal, and finally the "link" is the link to the recipe website with steps to make the meal and
+        "recipesInstructions" should be the list of instructions to make the meal.
         You should return in the following JSON format:
         {
             info:[
@@ -55,17 +59,19 @@ export default function Page() {
                     "name": str,
                     "image": str,
                     "description": str,
-                    "link": str
+                    "link": str,
+                    "recipesInstructions": [str]
                 }
             ]
         }
-    `
+    `;
 
-    const additionalIngredientRecipePrompt = `
+  const additionalIngredientRecipePrompt = `
         You are a food specialist, you take in all of the spices and ingredients and their respective expiration dates and return a list of recipes that can be made with these ingredients.
         Try to prioritize including the items with the earliest expiration dates but this is not a hard requirement, you must choose an ingredient from the ones listed but you can also add any additional ingredients you want.
         The "name" should be the name of the meal, the "image" should be the image of the meal in the form of the image address that can be used in a Image tag, the "description" should be the description of the meal,
-        the "additionalIngredients" should be the comma separated string of additional ingredients needed for this meal, and finally the "link" is the link to the recipe website with steps to make the meal.
+        the "additionalIngredients" should be the comma separated string of additional ingredients needed for this meal, and finally the "link" is the link to the recipe website with steps to make the meal and
+        "recipesInstructions" should be the list of instructions to make the meal.
         You should return in the following JSON format:
         {
             info:[
@@ -74,192 +80,247 @@ export default function Page() {
                     "image": str,
                     "description": str,
                     "additionalIngredients": str,
-                    "link": str
+                    "link": str,
+                    "recipesInstructions": [str]
                 }
             ]
         }
-    `
+    `;
 
-    useEffect(() => {
-        const fetchIngredients = async () => {
-            if (!isLoaded || !isSignedIn || !user) {
-                return;
-            }
+  useEffect(() => {
+    const fetchIngredients = async () => {
+      if (!isLoaded || !isSignedIn || !user) {
+        return;
+      }
 
-            const docRef = doc(collection(db, 'users'), user?.id);
-            const docSnap = await getDoc(docRef);
+      const docRef = doc(collection(db, "users"), user?.id);
+      const docSnap = await getDoc(docRef);
 
-            if (docSnap.exists()) {
-                setIngredients(docSnap.data().ingredients || {});
-            }
+      if (docSnap.exists()) {
+        setIngredients(docSnap.data().ingredients || {});
+      }
 
-            if (docSnap.exists() && docSnap.data().spices) {
-                const spicesObj = docSnap.data().spices;
-                const activeSpices = Object.entries(spicesObj)
-                    .filter(([_, value]) => value === true)
-                    .map(([key, _]) => key);
-                setSpiceStates(activeSpices);
-            }
-        };
-
-        fetchIngredients();
-    }, [isLoaded, isSignedIn, user]);
-
-    const handleClick = () => {
-        setShowRecipes(true);
+      if (docSnap.exists() && docSnap.data().spices) {
+        const spicesObj = docSnap.data().spices;
+        const activeSpices = Object.entries(spicesObj)
+          .filter(([_, value]) => value === true)
+          .map(([key, _]) => key);
+        setSpiceStates(activeSpices);
+      }
     };
 
-    const handleRecipeGeneration = async (systemPrompt: string, userPrompt: string) => {
-        const requestBody = {
-            systemPrompt: systemPrompt,
-            userPrompt: userPrompt
-        };
+    fetchIngredients();
+  }, [isLoaded, isSignedIn, user]);
 
-        const response = await fetch('/api/generate', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(requestBody),
-        });
+  const handleClick = () => {
+    setShowRecipes(true);
+  };
 
-        const data = await response.json();
-        return data;
+  const handleRecipeGeneration = async (
+    systemPrompt: string,
+    userPrompt: string
+  ) => {
+    const requestBody = {
+      systemPrompt: systemPrompt,
+      userPrompt: userPrompt,
     };
 
-    const createRecipeParameters = async (additional: boolean) => {
-        try {
-            const ingredientList = Object.entries(ingredients).map(([name, data]) => 
-                `${name}: ${data.expirationDate}`
-            );
-    
-            if (ingredientList.length === 0) {
-                setError('No ingredients available to generate recipes.');
-                return;
-            }
-    
-            const userPrompt = [...ingredientList, ...spiceStates].join(', ');
-            let recipeData;
-    
-            if (additional) {
-                recipeData = await handleRecipeGeneration(additionalIngredientRecipePrompt, userPrompt);
-            } else {
-                recipeData = await handleRecipeGeneration(currentIngredientRecipePrompt, userPrompt);
-            }
-    
-            // Ensure recipeData is in the correct format
-            if (!recipeData.info || !Array.isArray(recipeData.info)) {
-                throw new Error('Recipe data is not in correct format');
-            }
-    
-            setData(recipeData.info);
-            setError('');
-            setShowRecipes(true);
-        } catch (err) {
-            setError('Failed to generate recipes');
-            setData([]);
-        }
+    const response = await fetch("/api/generate", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(requestBody),
+    });
+
+    const data = await response.json();
+    return data;
+  };
+
+  const createRecipeParameters = async (additional: boolean) => {
+    try {
+      const ingredientList = Object.entries(ingredients).map(
+        ([name, data]) => `${name}: ${data.expirationDate}`
+      );
+
+      if (ingredientList.length === 0) {
+        setError("No ingredients available to generate recipes.");
+        return;
+      }
+
+      const userPrompt = [...ingredientList, ...spiceStates].join(", ");
+      let recipeData;
+
+      if (additional) {
+        recipeData = await handleRecipeGeneration(
+          additionalIngredientRecipePrompt,
+          userPrompt
+        );
+      } else {
+        recipeData = await handleRecipeGeneration(
+          currentIngredientRecipePrompt,
+          userPrompt
+        );
+      }
+
+      // Ensure recipeData is in the correct format
+      if (!recipeData.info || !Array.isArray(recipeData.info)) {
+        throw new Error("Recipe data is not in correct format");
+      }
+
+      setData(recipeData.info);
+      setError("");
+      setShowRecipes(true);
+    } catch (err) {
+      setError("Failed to generate recipes");
+      setData([]);
     }
+  };
 
-    return (
-        <div style={{ minHeight: '100vh', width: '100%', backgroundColor: '#3D5926' }}>
-            <Navbar />
-            <Typography 
-                style={{ fontFamily: oooh_baby.style.fontFamily, color: '#f0e68c', textAlign: 'center', fontSize: '60px' }}
+  return (
+    <div
+      style={{ minHeight: "100vh", width: "100%", backgroundColor: "#3D5926" }}
+    >
+      <Navbar />
+      <Typography
+        style={{
+          fontFamily: oooh_baby.style.fontFamily,
+          color: "#f0e68c",
+          textAlign: "center",
+          fontSize: "60px",
+        }}
+      >
+        Recipes
+      </Typography>
+
+      <Box
+        sx={{
+          margin: "2px 40px",
+          padding: "10px",
+          backgroundColor: "#FDEDD6",
+          color: "black",
+          border: "3px solid black",
+          borderRadius: "8px",
+        }}
+      >
+        <Typography
+          variant="h6"
+          sx={{
+            marginLeft: "8px",
+            marginTop: "8px",
+            textAlign: "center",
+            fontFamily: jost.style.fontFamily,
+          }}
+        >
+          Ingredients and Expiration Dates
+        </Typography>
+
+        <Box sx={{ padding: "16px", minHeight: "75px", overflow: "auto" }}>
+          <Box
+            sx={{ display: "flex", overflowX: "auto", whiteSpace: "nowrap" }}
+          >
+            {Object.entries(ingredients).map(([ingredient, data]) => (
+              <Box
+                key={ingredient}
+                sx={{ minWidth: "100px", marginRight: "16px" }}
+              >
+                <Stack>
+                  <Typography>{ingredient}</Typography>
+                  <Typography>{data.purchaseDate}</Typography>
+                </Stack>
+              </Box>
+            ))}
+          </Box>
+        </Box>
+      </Box>
+
+      <Box>
+        {error ? (
+          <Alert
+            sx={{ margin: "2px 40px", border: "3px solid black" }}
+            severity="error"
+          >
+            {error}
+          </Alert>
+        ) : (
+          showRecipes && (
+            <Box
+              sx={{
+                margin: "2px 40px",
+                backgroundColor: "#FDEDD6",
+                color: "black",
+                border: "3px solid black",
+                borderRadius: "8px",
+              }}
             >
-                Recipes
-            </Typography>
-
-            <Box 
-                sx={{ 
-                    margin: '2px 40px', 
-                    padding: '10px', 
-                    backgroundColor: '#FDEDD6',
-                    color: 'black',
-                    border: '3px solid black',
-                    borderRadius: '8px'
+              <Typography
+                variant="h6"
+                sx={{
+                  marginLeft: "8px",
+                  marginTop: "8px",
+                  textAlign: "center",
+                  fontFamily: jost.style.fontFamily,
                 }}
-            >
-                <Typography variant="h6" sx={{ marginLeft: '8px', marginTop: '8px', textAlign: 'center', fontFamily: jost.style.fontFamily }}>
-                    Ingredients and Expiration Dates
-                </Typography>
-
-                <Box sx={{ padding: '16px', minHeight: '75px', overflow: 'auto' }}>
-                    <Box sx={{ display: 'flex', overflowX: 'auto', whiteSpace: 'nowrap' }}>
-                    {Object.entries(ingredients).map(([ingredient, data]) => (
-                        <Box key={ingredient} sx={{ minWidth: '100px', marginRight: '16px' }}>
-                            <Stack>
-                                <Typography>{ingredient}</Typography>
-                                <Typography>{data.purchaseDate}</Typography>
-                            </Stack>
-                        </Box>
-                    ))}
-                    </Box>
-                </Box>
-            </Box>
-            
-            <Box>
-                {error ? (<Alert sx={{margin: '2px 40px', border: '3px solid black'}} severity="error">{error}</Alert>) :
-                showRecipes && (
-                <Box 
-                    sx={{ 
-                        margin: '2px 40px', 
-                        backgroundColor: '#FDEDD6', 
-                        color: 'black',
-                        border: '3px solid black',
-                        borderRadius: '8px',
-                    }}
+              >
+                Recipes
+              </Typography>
+              <Box
+                sx={{ padding: "16px", minHeight: "340px", overflow: "auto" }}
+              >
+                <Box
+                  sx={{
+                    display: "flex",
+                    overflowX: "auto",
+                    whiteSpace: "nowrap",
+                    padding: "0px",
+                  }}
                 >
-                    <Typography variant="h6" sx={{ marginLeft: '8px', marginTop: '8px', textAlign: 'center', fontFamily: jost.style.fontFamily }}>
-                        Recipes
-                    </Typography>
-                    <Box sx={{ padding: '16px', minHeight: '340px', overflow: 'auto' }}>
-                        <Box sx={{ display: 'flex', overflowX: 'auto', whiteSpace: 'nowrap', padding: '0px' }}>
-                            <Stack direction="row" sx={{ padding: '8px' }}>
-                                {data?.map((recipe, index) => (
-                                    <RecipeCard
-                                        key={index}
-                                        title={recipe.name}
-                                        image={recipe.image}
-                                        description={recipe.description}
-                                        link={recipe.link}
-                                        additionalIngredients={recipe.additionalIngredients}
-                                    />
-                                ))}
-                            </Stack>
-                        </Box>
-                    </Box>
+                  <Stack direction="row" sx={{ padding: "8px" }}>
+                    {data?.map((recipe, index) => (
+                      <RecipeCard
+                        key={index}
+                        title={recipe.name}
+                        image={recipe.image}
+                        description={recipe.description}
+                        link={recipe.link}
+                        additionalIngredients={recipe.additionalIngredients}
+                        recipesInstructions={recipe.recipesInstructions}
+                      />
+                    ))}
+                  </Stack>
                 </Box>
-                )}
-
-                <Box style={{ textAlign: 'center', padding: '16px' }}>
-                    <Stack direction='row' justifyContent={'space-evenly'}>
-                        <Button 
-                            sx={{ 
-                                backgroundColor: '#B37238',
-                                color: '#FDEDD6',
-                                fontFamily: jost.style.fontFamily,
-                                '&:hover': { backgroundColor: '#9e5e24' } 
-                            }} 
-                            onClick={() => createRecipeParameters(false)}
-                        >
-                            Generate Recipe with Current Ingredients
-                        </Button>
-                        <Button 
-                            sx={{ 
-                                backgroundColor: '#B37238',
-                                color: '#FDEDD6',
-                                fontFamily: jost.style.fontFamily,
-                                '&:hover': { backgroundColor: '#9e5e24' } 
-                            }} 
-                            onClick={() => createRecipeParameters(true)}
-                        >
-                            Suggest Recipe with Additional Ingredients
-                        </Button>
-                    </Stack> 
-                </Box>
+              </Box>
             </Box>
-        </div>
-    );
-}
+          )
+        )}
 
+        <Box style={{ textAlign: "center", padding: "16px" }}>
+          <Stack direction="row" justifyContent={"space-evenly"}>
+            <Button
+              sx={{
+                backgroundColor: "#B37238",
+                color: "#FDEDD6",
+                fontFamily: jost.style.fontFamily,
+                "&:hover": { backgroundColor: "#9e5e24" },
+              }}
+              onClick={() => createRecipeParameters(false)}
+            >
+              Generate Recipe with Current Ingredients
+            </Button>
+            <Button
+              sx={{
+                backgroundColor: "#B37238",
+                color: "#FDEDD6",
+                fontFamily: jost.style.fontFamily,
+                "&:hover": { backgroundColor: "#9e5e24" },
+              }}
+              onClick={() => createRecipeParameters(true)}
+            >
+              Suggest Recipe with Additional Ingredients
+            </Button>
+          </Stack>
+        </Box>
+      </Box>
+    </div>
+  );
+}
--- src/app/generate-recipes/recipeCard.tsx
+++ src/app/generate-recipes/recipeCard.tsx
@@ -1,51 +1,102 @@
-import { Stack, Box, Typography } from "@mui/material";
+import { Stack, Box, Typography, Button, Modal, Divider } from "@mui/material";
 import { Jost } from "next/font/google";
+import { useState } from "react";
 
 interface RecipeProps {
-    title: string;
-    image: string;
-    description: string;
-    link: string;
-    additionalIngredients?: string;
+  title: string;
+  image: string;
+  description: string;
+  link: string;
+  additionalIngredients?: string;
+  recipesInstructions?: string[];
 }
 
 const jost = Jost({
-    weight: ['300', '400', '500'],
-    subsets: ['latin'],
+  weight: ["300", "400", "500"],
+  subsets: ["latin"],
 });
 
-const RecipeCard = ({ title, image, description, link, additionalIngredients }: RecipeProps) => (
-    <Box sx={{ padding: '16px' }}>
-        <Box 
-            className="bg-themeBlue rounded-md text-black p-2"
-            minHeight={290} 
-            maxHeight={250} 
-            minWidth={375} 
-            maxWidth={375} 
+const RecipeCard = ({
+  title,
+  image,
+  description,
+  link,
+  additionalIngredients,
+  recipesInstructions,
+}: RecipeProps) => {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  return (
+    <Box sx={{ padding: "16px" }}>
+      <Box
+        className="bg-themeBlue rounded-md text-black p-2"
+        minHeight={290}
+        maxHeight={250}
+        minWidth={375}
+        maxWidth={375}
+        sx={{
+          overflowY: "auto",
+          wordWrap: "break-word",
+          wordBreak: "break-word",
+          whiteSpace: "normal",
+          border: "1.5px solid #000",
+          borderRadius: "8px",
+        }}
+      >
+        <Stack justifyContent={"space-between"} spacing={2}>
+          <Typography
             sx={{
-                overflowY: 'auto',
-                wordWrap: 'break-word',
-                wordBreak: 'break-word',
-                whiteSpace: 'normal',
-                border: '1.5px solid #000',
-                borderRadius: '8px'
+              marginTop: "-2.7rem",
+              fontFamily: jost.style.fontFamily,
+              fontWeight: 400,
+              fontSize: "1.3rem",
             }}
+          >
+            {title}
+          </Typography>
+          {/* <img src={image} alt={title} /> */}
+          <Typography>Description: {description}</Typography>
+          {additionalIngredients && (
+            <Typography>
+              Additional Ingredients Needed: {additionalIngredients}
+            </Typography>
+          )}
+          <Typography>Link: {link}</Typography>
+          <Button variant="contained" onClick={handleOpen}>
+            See More
+          </Button>
+        </Stack>
+      </Box>
+      <Modal open={open} onClose={handleClose}>
+        <Box
+          sx={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            width: 400,
+            bgcolor: "background.paper",
+            border: "2px solid #000",
+            boxShadow: 24,
+            p: 4,
+            color: "black",
+          }}
         >
-            <Stack justifyContent={"space-between"} spacing={2}>
-                <Typography sx={{marginTop: "-2.7rem", fontFamily: jost.style.fontFamily, fontWeight: 400, fontSize: "1.3rem"}}>
-                    {title}
-                </Typography>
-                {/* <img src={image} alt={title} /> */}
-                <Typography>Description: {description}</Typography>
-                {additionalIngredients && (
-                    <Typography>
-                        Additional Ingredients Needed: {additionalIngredients}
-                    </Typography>
-                )}
-                <Typography>Link: {link}</Typography>
-            </Stack>
+          <Typography variant="h6" component="h2" sx={{ mb: 2, textAlign: "center" }}>
+            Recipe Instructions
+            </Typography>
+          <Divider sx={{ mb: 2}} />
+          <ol style={{ listStyleType: "decimal", paddingLeft: "20px" }}>
+            {recipesInstructions?.map((instruction, index) => (
+              <li key={index}>{instruction}</li>
+            ))}
+          </ol>
         </Box>
+      </Modal>
     </Box>
-);
+  );
+};
 
 export default RecipeCard;
